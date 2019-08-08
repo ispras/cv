f6dc04e0f7eaacce29abae42d6b74ba49d34cc3c
Vitaly Mordan <mordan@ispras.ru>
2019-08-08 13:37:44 +0300

    Add comment for main launch script

--- scripts/launch.py
+++ scripts/launch.py
@@ -5,6 +5,9 @@ import argparse
 from components.launcher import Launcher
 
 if __name__ == '__main__':
+    """
+    Launch a whole verification process for the given system using config files. 
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", "-c", help="list of config files", nargs="+", required=True)
     options = parser.parse_args()
