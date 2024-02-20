6fd989ea85e080cbedb0551fccf8de381114142d
vmordan <mordan@ispras.ru>
2024-02-20 22:51:19 +0800

    Add support for Klever EMG comments in JSON format (#55)
    * Add initial support for EMG comments in JSON format

--- scripts/mea/et/internal_witness.py
+++ scripts/mea/et/internal_witness.py
@@ -61,6 +61,7 @@ class InternalWitness:
         self._model_funcs = {}
         self._spec_funcs = {}
         self._env_models = {}
+        self._env_models_json = {}
         self._notes = {}
         self._asserts = {}
         self._actions = []
@@ -259,6 +260,7 @@ class InternalWitness:
                                        f"'{self._resolve_function(func_id)}'")
                     edge['note'] = self.process_comment(note)
                 if func_id in self._env_models:
+                    # TODO: not supported anymore
                     env_note = self._env_models[func_id]
                     self._logger.debug(f"Add note {env_note} for environment function '"
                                        f"{self._resolve_function(func_id)}'")
@@ -268,6 +270,11 @@ class InternalWitness:
                 note = self._notes[file_id][start_line]
                 self._logger.debug(f"Add note {note} for statement from '{file}:{start_line}'")
                 edge['note'] = self.process_comment(note)
+            elif file_id in self._env_models_json and start_line in self._env_models_json[file_id]:
+                env = self._env_models_json[file_id][start_line]
+                self._logger.debug(f"Add EMG comment '{env}' for operation from '{file}:{start_line}'")
+                edge['env'] = self.process_comment(env)
+                del self._env_models_json[file_id][start_line]
             elif file_id in self._asserts and start_line in self._asserts[file_id]:
                 warn = self._asserts[file_id][start_line]
                 self._logger.debug(f"Add warning {warn} for statement from '{file}:{start_line}'")
@@ -293,6 +300,7 @@ class InternalWitness:
     def _parse_model_comments(self):
         self._logger.info('Parse model comments from source files referred by witness')
         emg_comment = re.compile(r'/\*\sLDV\s(.*)\s\*/')
+        emg_comment_json = re.compile(r'/\*\sEMG_ACTION\s({.*})\s\*/')
 
         for file_id, file in self.files:
             if not os.path.isfile(file):
@@ -311,6 +319,16 @@ class InternalWitness:
                     if match:
                         data = json.loads(match.group(1))
                         self._add_emg_comment(file_id, line, data)
+
+                    # Try match JSON EMG comment
+                    match = emg_comment_json.search(text)
+                    if match:
+                        data = json.loads(match.group(1))
+                        if "comment" in data:
+                            if file_id not in self._env_models_json:
+                                self._env_models_json[file_id] = {}
+                            self._env_models_json[file_id][line + 1] = data["comment"]
+                            # TODO: parse other arguments as well
 
                     # Match rest comments
                     match = re.search(
@@ -414,7 +432,7 @@ class InternalWitness:
                     'enter': self.add_function(entry_point),
                     'start line': 0,
                     'file': 0,
-                    'env': 'entry point',
+                    'entry_point': 'entry point',
                     'source': f"{entry_point}()"
                 }
                 if not self._threads:
