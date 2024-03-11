9eaa9671d7bd34f28813ca899710cafee30df1ec
alekseevk1 <73096235+alekseevk1@users.noreply.github.com>
2024-03-11 17:37:05 +0300

    delete trailing whitespace (#58)

--- scripts/mea/et/internal_witness.py
+++ scripts/mea/et/internal_witness.py
@@ -273,7 +273,6 @@ class InternalWitness:
             elif file_id in self._env_models_json and start_line in self._env_models_json[file_id]:
                 env = self._env_models_json[file_id][start_line]
                 comment = env["comment"]
-                
                 relevant = env.get("relevant", False)
 
                 #TODO add remaining
