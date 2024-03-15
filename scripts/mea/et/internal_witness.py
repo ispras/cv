dad146d37f480d07f495aad1dfd54a0199f4df71
alekseevk1 <alekseev.nikolai.a@phystech.edu>
2024-03-15 13:53:48 +0300

    add structure for collecting all information for action types: fix lines tag

--- scripts/mea/et/internal_witness.py
+++ scripts/mea/et/internal_witness.py
@@ -284,19 +284,19 @@ class InternalWitness:
                     action_begin_dict = {}
                     action_begin_dict['env'] = comment
                     action_begin_dict['file'] = file_id
-                    action_begin_dict['source'] = edge.get("source", None)
+                    action_begin_dict['source'] = edge["source"]
                     action_begin_dict['enter'] = self.add_function(name)
                     action_begin_dict['start line'] = start_line
                     action_type_data[name].append(action_begin_dict)
                 if action_type == "ACTION_END":
                     action_end_dict = {}
                     action_end_dict['file'] = file_id
-                    action_end_dict['start line'] = action_type_data[name][0]['start_line']
+                    action_end_dict['start line'] = start_line
                     action_end_dict['end line'] = start_line
-                    action_begin_dict['end_line'] = start_line
-                    action_end_dict['source'] = edge.get("source", None)
+                    action_end_dict['source'] = edge["source"]
                     action_end_dict['return'] = self.add_function(name)
                     action_type_data[name].append(action_end_dict)
+                self._logger.warning(edge)
 
                 #TODO add remaining
                 self._logger.debug(f"Add EMG comment '{comment}' for operation from '{file}:{start_line}'")
